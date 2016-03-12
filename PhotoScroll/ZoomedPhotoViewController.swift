87f453ceb357b02b7dc3df1860c06c10fc29f827
ElijahButers <Elijahbuters@gmail.com>
2016-03-12 11:31:13 +0200

    Added func updateConstraintsForSize allowing image to be pinned at the top of view, fixing other issues.

--- PhotoScroll/ZoomedPhotoViewController.swift
+++ PhotoScroll/ZoomedPhotoViewController.swift
@@ -35,21 +35,40 @@ class ZoomedPhotoViewController: UIViewController {
   
   override func viewDidLoad() {
     imageView.image = UIImage(named: photoName)
+  }
+  
+  override func viewDidLayoutSubviews() {
+    super.viewDidLayoutSubviews()
     
     updateMinZoomScaleForSize(view.bounds.size)
+    
   }
+  
   
   private func updateMinZoomScaleForSize(size: CGSize) {
     
     let widthScale = size.width / imageView.bounds.width
-    let heightScale = size.width / imageView.bounds.height
+    let heightScale = size.height / imageView.bounds.height
     let minScale = min(widthScale, heightScale)
     
     scrollView.minimumZoomScale = minScale
     scrollView.zoomScale = minScale
   }
   
+  private func updateConstraintsForSize(size: CGSize) {
+    
+    let yOffset = max(0, (size.height - imageView.frame.height) / 2)
+    imageViewTopConstraint.constant = yOffset
+    imageViewBottomConstraint.constant = yOffset
+    
+    let xOffset = max(0, (size.width - imageView.frame.width) / 2)
+    imageViewLeadingConstraint.constant = xOffset
+    imageViewTrailingConstraint.constant = xOffset
+    
+    view.layoutIfNeeded()
+  }
 }
+
 
 extension ZoomedPhotoViewController: UIScrollViewDelegate {
   func viewForZoomingInScrollView(scrolView: UIScrollView) -> UIView? {
